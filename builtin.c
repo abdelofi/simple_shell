ee9acb0de0847d11f15207c4d095c72654917bec
el oufir <eloufirabdelaati@gmail.com>
2023-07-26 14:54:40 -0700

    updated

--- builtin.c
+++ builtin.c
@@ -1,16 +1,17 @@
 #include "shell.h"
 
 /**
- * _myexit - Exits the shell.
- * @info: Structure containing potential arguments.
- *
- * Return: Exits with a given exit status (0) if info->argv[0] != "exit".
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: exits with a given exit status
+ * (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1]) /* If there is an exit argument */
+	if (info->argv[1]) /* If there is an exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
@@ -29,10 +30,10 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - Changes the current directory of the process.
- * @info: Structure containing potential arguments.
- *
- * Return: Always 0.
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
  */
 int _mycd(info_t *info)
 {
@@ -59,8 +60,7 @@ int _mycd(info_t *info)
 			_putchar('\n');
 			return (1);
 		}
-		_puts(_getenv(info, "OLDPWD="));
-		_putchar('\n');
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
 		chdir_ret = /* TODO: what should this be? */
 			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
@@ -69,8 +69,7 @@ int _mycd(info_t *info)
 	if (chdir_ret == -1)
 	{
 		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]);
-		_eputchar('\n');
+		_eputs(info->argv[1]), _eputchar('\n');
 	}
 	else
 	{
@@ -81,10 +80,10 @@ int _mycd(info_t *info)
 }
 
 /**
- * _myhelp - Displays help information.
- * @info: Structure containing potential arguments.
- *
- * Return: Always 0.
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
  */
 int _myhelp(info_t *info)
 {
@@ -96,4 +95,3 @@ int _myhelp(info_t *info)
 		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
-
